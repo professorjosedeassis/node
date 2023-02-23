4cd133cf7af406c57375efb06bd09e6e496db0c9
Professor José de Assis <josedeassisfilho@gmail.com>
2023-02-23 13:42:15 -0300

    Fundamentos (sintaxe e variaveis)

---
+++ fundamentos/sintaxe.js
@@ -0,0 +1,12 @@
+/**
+ * Tutorial Node.js
+ * Sintaxe básica
+ */
+
+//Sentenças
+console.log("Sintaxe básica")
+
+//Blocos
+{
+    console.log("Blocos")
+}
---
+++ fundamentos/variaveis.js
@@ -0,0 +1,30 @@
+/**
+ * Tutorial Node.js
+ * Variáveis
+ */
+
+console.log("_______________________________const")
+const nome = "José de Assis"
+console.log(nome)
+console.log(typeof nome)
+const pi = 3.14
+console.log(pi)
+console.log(typeof pi)
+const vip = true
+console.log(vip)
+console.log(typeof vip)
+//pi = 3.1415
+console.log("_______________________________var")
+var time = "Corinthians"
+console.log(time)
+time = "Palmeiras"
+console.log(time)
+//problema no uso do var para declarar uma variável
+var time = "São Paulo"
+console.log(time)
+console.log("_______________________________let")
+let linguagem = "Java"
+console.log(linguagem)
+linguagem = "JavaScript"
+console.log(linguagem)
+//let linguagem = "Python"
