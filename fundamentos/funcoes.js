0367197d6e9d7baeb8d448e5534e49c92c244ea6
Professor José de Assis <josedeassisfilho@gmail.com>
2023-07-19 11:41:55 -0300

    add files via update

---
+++ fundamentos/funcoes.js
@@ -0,0 +1,56 @@
+/**
+ * Tutorial Node.js
+ * Funções
+ */
+
+function hello() {
+    console.log("Hello Function")
+}
+
+hello()
+console.log(typeof hello)
+
+const hello2 = function() {
+    console.log("Hello Function Assigned")
+}
+
+hello2()
+console.log(typeof hello2)
+
+const hello3 = () => {
+    console.log("Hello Arrow Function Assigned")
+}
+
+hello3()
+console.log(typeof hello3)
+
+const hello4 = _ => console.log("Hello Arrow Function Assigned Simplified")
+
+hello4()
+console.log(typeof hello4)
+
+function somarS (num1, num2) {
+    return (console.log(num1 + num2))
+}
+
+somarS(2,3)
+
+let somarA = function (num1, num2) {
+    return (console.log(num1 + num2))
+}
+
+somarA(2,3)
+console.log(typeof somarA)
+
+let somarAF = (num1, num2) => {
+    return (console.log(num1 + num2))
+}
+
+somarAF(2,3)
+console.log(typeof somarAF)
+
+let somarAFS = (num1, num2) => (console.log(num1 + num2))
+somarAFS(2,3)
+console.log(typeof somarAFS)
+
+
---
+++ fundamentos/sintaxe2.js
@@ -0,0 +1,22 @@
+/**
+ * Tutorial Node.js
+ * Uso de chaves nas estruturas de controle
+ */
+
+// Nas estruturas de controle condicional e nos laços de repetições
+// podemos omitir as chaves, porém só uma linha de código é vinculada
+
+let media = 3
+
+if (media < 5)
+    console.log("REPROVADO")
+else
+    console.log("APROVADO")
+console.log("Emitir certificado")
+
+for (let x = 1; x < 10; x++)
+    console.log(x)
+console.log("node.js")
+
+
+
