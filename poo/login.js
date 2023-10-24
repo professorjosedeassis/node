cf79f0ae3c12ab526641759d7bf8429e7d1e2455
Professor José de Assis <josedeassisfilho@gmail.com>
2023-10-24 12:39:46 -0300

    upload files

---
+++ poo/login.js
@@ -0,0 +1,34 @@
+/**
+ * Fundamentos da POO
+ * Encapsulamento
+ */
+
+class User {
+    //atributos
+    constructor(login, senha) {
+        this.login = login
+        //encapsulamento
+        let _senha = senha
+        this.getSenha = () => _senha
+        this.setSenha = (novaSenha) => _senha = novaSenha
+    }
+    //ação
+    logar() {
+        console.log("______________________________")
+        console.log(`Usuário: ${this.login}`)
+        console.log(`Senha: ${this.getSenha()}`)
+        if (this.login === "admin" && this.getSenha() === "123456") {
+            console.log("Usuário autenticado")
+        } else {
+            console.log("usuário e/ou senha inválido(s)")
+        }
+    }
+}
+
+/** SISTEMA **/
+console.clear()
+const user1 = new User("admin","1234")
+user1.logar()
+user1.setSenha("123456")
+user1.logar()
+
---
+++ poo/minecraft.js
@@ -0,0 +1,89 @@
+/**
+ * Fundamentos da POO
+ * Abstração - Herança - Polimorfismo 
+ */
+
+class Bloco {
+    //atributos
+    constructor(textura, resistencia) {
+        this.textura = textura
+        this.resistencia = resistencia
+    }
+    //ações
+    criarBloco() {
+        console.log("-------------------------------------------------------")
+        console.log("┌─┐")
+        console.log("└─┘")
+        console.log(`Bloco de ${this.textura}`)
+        console.log(`Resistência: ${this.resistencia}`)
+    }
+
+    construir() {
+        console.log(`Bloco de ${this.textura} colocado.`)
+    }
+
+    minerar() {
+        console.log("□ □ □ □ Recursos obtidos!")
+    }
+}
+
+class Enxada extends Bloco {
+    //atributos
+    constructor(textura, resistencia, conquista) {
+        super(textura, resistencia)
+        this.conquista = conquista
+    }
+    //ações
+    criarEnxada() {
+        console.log("-------------------------------------------------------")
+        console.log("-_")
+        console.log(" /")
+        console.log(`Enxada de ${this.textura}`)
+        console.log(`Resistência: ${this.resistencia}`)
+    }
+
+    arar() {
+        console.log("._._._. Terra arada!")
+        if (this.conquista === true) {
+            console.log("☀ Conquista obtida!")
+        }
+    }
+
+    //polimorfismo
+    minerar() {
+        console.log("✞ Dano atribuído!") 
+    }
+}
+
+/******* MUNDO *******/
+console.clear()
+console.log(" _____ _____ _____ _____ _____ _____ _____ _____ _____ ")
+console.log("|     |     |   | |   __|     | __  |  _  |   __|_   _|")
+console.log("| | | |-   -| | | |   __|   --|    -|     |   __| | |  ")
+console.log("|_|_|_|_____|_|___|_____|_____|__|__|__|__|__|    |_|  ")
+
+console.log("")
+
+//instanciando um objeto
+const bloco1 = new Bloco("terra", 1)
+bloco1.criarBloco()
+
+const bloco2 = new Bloco("madeira", 2)
+bloco2.criarBloco()
+bloco2.construir()
+
+const bloco3 = new Bloco("pedra", 5)
+bloco3.criarBloco()
+bloco3.minerar()
+
+const enxada1 = new Enxada("madeira", 2, false)
+enxada1.criarEnxada()
+enxada1.arar()
+
+const enxada2 = new Enxada("ferro", 5, true)
+enxada2.criarEnxada()
+enxada2.arar()
+
+const enxada3 = new Enxada("diamante", 10, false)
+enxada3.criarEnxada()
+enxada3.minerar()
